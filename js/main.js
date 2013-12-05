9b5b73df328ed9f061843b0b31f923bb9fada19d
CodeKevin <kevin@codekevin.com>
2013-12-05 02:51:05 -0500

    Swapping CodeMirror with Ace
    Ace is the sauce.

--- js/main.js
+++ js/main.js
@@ -3,55 +3,15 @@
 	{
 	    return new Array( num + 1 ).join( this );
 	}
-// CodeMirror CloseTags plugin
-	CodeMirror.commands.autocomplete = function(cm) {
-	    CodeMirror.showHint(cm, CodeMirror.htmlHint);
-	}
-
-
-// CodeMirror initialization
-	var css = CodeMirror.fromTextArea(document.getElementById('css'), {
-		mode: 'css', 
-		theme: 'monokai',
-		lineNumbers: true
-	});
-
-	var html = CodeMirror.fromTextArea(document.getElementById("html"), {
-		mode: 'text/html',
-		autoCloseTags: true,
-		extraKeys: {"Shift-Tab": "autocomplete"},
-		theme: "monokai",
-		lineNumbers: true
-	});
-
-
-// onChange Handler for CodeMirror. Updates iFrame
-	css.on("change", function(cm, change) {
-		// $('#render').contents().find('html').html('<style>'+css.getValue()+'</style>' + html.getValue()); 
-		var content = '<style>'+css.getValue()+'</style>' + html.getValue();
-		var iframe = document.getElementById('render');
-		iframe.contentWindow.document.open('text/html', 'replace');
-		iframe.contentWindow.document.write(content);
-		iframe.contentWindow.document.close();
-	});			
-
-	html.on("change", function(cm, change) {
-		// $('#render').contents().find('html').html('<style>'+css.getValue()+'</style>' + html.getValue()); 
-		var content = '<style>'+css.getValue()+'</style>' + html.getValue();
-		var iframe = document.getElementById('render');
-		iframe.contentWindow.document.open('text/html', 'replace');
-		iframe.contentWindow.document.write(content);
-		iframe.contentWindow.document.close();
-	});
 
 // Handles adding the javascript include on check and removing it
 	$('.include').change(function(){
 		if(!$(this).is(':checked')){
-			for (var i = 0; i < html.lineCount(); i++) {
-				var x = html.getLine(i).indexOf($(this).val());
+			for (var i = 0; i < html.session.getLength(); i++) {
+				var x = html.session.getLine(i).indexOf($(this).val());
 
 				if(x != -1){
-					html.removeLine(i);
+					html.session.getDocument().removeLines(i, i);
 					return;
 				}
 			};
@@ -60,16 +20,15 @@
 		var x = '';
 		var i = 0;
 
-		for (; i < html.lineCount(); i++) {
-			x = html.getLine(i).indexOf('<head>');
+		for (; i < html.session.getLength(); i++) {
+			x = html.session.getLine(i).indexOf('<head>');
 			if(x != -1){
 				break;
 			}
 		};
 
-
-		count = (html.getLine(i).split(" ").length - 1)/2
-		html.replaceRange( "\n"+("  ".repeat(count + 1))+$(this).val(), {line: i, ch: x+6});
+		count = (html.session.getLine(i).split(" ").length - 1)/2
+		html.session.getDocument().insertLines(x, Array(("  ".repeat(count + 1))+$(this).val()));
 	})
 
 // Handles iFrame content on load and on resize
